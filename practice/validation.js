9b170684d2b43c77df6ee6939118621d9c78745e
Chijioke <wisestcodes@gmail.com>
2023-03-01 10:37:44 -0500

    validate form fields

--- practice/validation.js
+++ practice/validation.js
@@ -1,3 +1,24 @@
+const inputs = document.querySelectorAll('input');
 const patterns = {
-    telephone: /^\d{11}$/
+    phone: /^\d{11}$/,
+    f_Name: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@_]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 }
+
+// validation function
+function validate(field, regex) {
+    if(regex.test(field.value) == true) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        // console.log(e.target, e.target.nextElementSibling);
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
